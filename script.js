18ced6cb53a4ce385d331d9eec9cb1e841d8d1f3
David Boyd <dboyd2005@msn.com>
2024-07-05 14:35:23 +0100

    Add behaviour to calc UI elements
    Appropriate calculator mechanical response from user stimulus.

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-let numOperand1 = 0;
-let numOperand2 = 0;
+let operand1 = 0;
+let operand2 = 0;
 let numOperator = "";
+let accumulatorAnswerDisplay = "";
+
+/*
+ * Referenced DOM nodes with variables for the use of
+ * event listeners.
+ */
+
+let keyboard = document.getElementById("keyboard");
+let metaScreen = document.getElementById("meta-screen");
+let accumulatorScreen = document.getElementById("accumulator-screen");
 
 /*
  * Setup event listeners (observers) after the page 'load' event fires
  * so that all page elements are loaded before any logic is executed.
  */
 addEventListener("load", (event) => {
-  add();
-  subtract();
-  multiply();
-  divide();
-
+  keyboard.addEventListener("click", (event) => {
+    let data = event.target.dataset.calculator;
+    parseDataKeys(data);
+  });
   console.log("The page is fully loaded.");
 });
 
@@ -19,6 +28,132 @@ addEventListener("load", (event) => {
  * Implemented controller logic to respond to calls
  * from event listeners with appropriate action(s) I.E. processing.
  */
+
+parseDataKeys = (data) => {
+  if (!isNaN(data)) {
+    parseOperand(+data);
+  } else if (data === "point") {
+    parseOperand(data);
+  } else if (data === "clear" || data === "delete") {
+    parseModifier(data);
+  } else if (
+    isNaN(data) &&
+    data !== "point" &&
+    data !== undefined &&
+    data !== "clear" &&
+    data !== "delete"
+  ) {
+    parseOperator(data);
+  } else if (data === undefined) {
+    console.log("miss click");
+  }
+};
+
+/*
+ * Helper functions.
+ */
+
+parseOperand = (operand) => {
+  if (typeof operand === "number") {
+    populateAccumulatorDisplay(operand);
+  } else if (operand === "point") {
+    disableDecimalPoint(true);
+    operand = ".";
+    populateAccumulatorDisplay(operand);
+  }
+};
+
+parseOperator = (operator) => {
+  disableOperators(true);
+  switch (operator) {
+    case "plus":
+      operator = " + ";
+      populateAccumulatorDisplay(operator);
+      disableDecimalPoint(false);
+      break;
+    case "minus":
+      operator = " − ";
+      populateAccumulatorDisplay(operator);
+      disableDecimalPoint(false);
+      break;
+    case "multiply":
+      operator = " × ";
+      populateAccumulatorDisplay(operator);
+      disableDecimalPoint(false);
+      break;
+    case "divide":
+      operator = " ÷ ";
+      populateAccumulatorDisplay(operator);
+      disableDecimalPoint(false);
+      break;
+    case "equals":
+      operator = " = ";
+      parseEquation();
+      disableOperators(false);
+      disableDecimalPoint(false);
+      break;
+  }
+};
+
+parseModifier = (modify) => {
+  switch (modify) {
+    case "clear":
+      resetCalc();
+      break;
+    case "delete":
+      deleteLastItem();
+      break;
+  }
+};
+
+populateAccumulatorDisplay = (target) => {
+  accumulatorAnswerDisplay += target;
+  accumulatorScreen.textContent = `${accumulatorAnswerDisplay}`;
+};
+
+resetCalc = () => {
+  console.log("calculator reset");
+  accumulatorScreen.textContent = "0";
+  accumulatorAnswerDisplay = "";
+  metaScreen.textContent = "Ans = 0";
+  disableDecimalPoint(false);
+  disableOperators(false);
+};
+
+deleteLastItem = () => {
+  if (accumulatorScreen.textContent) {
+    console.log(accumulatorScreen.textContent.length);
+    accumulatorAnswerDisplay = accumulatorAnswerDisplay.slice(0, -1);
+    accumulatorScreen.textContent = accumulatorAnswerDisplay;
+  }
+
+  if (!accumulatorScreen.textContent) {
+    console.log("I'm empty");
+    accumulatorScreen.textContent = "0";
+  }
+
+  //TODO:
+  //if remove decimal disableDecimalPoint(false);
+  //if remove blank space disableOperators(false) and remove 3 chars
+};
+
+disableDecimalPoint = (activeState) => {
+  document.querySelector('[data-calculator="point"]').disabled = activeState;
+};
+
+disableOperators = (activeState) => {
+  document.querySelector('[data-calculator="plus"]').disabled = activeState;
+  document.querySelector('[data-calculator="minus"]').disabled = activeState;
+  document.querySelector('[data-calculator="divide"]').disabled = activeState;
+  document.querySelector('[data-calculator="multiply"]').disabled = activeState;
+};
+
+parseEquation = () => {
+  console.log("parse the equation");
+  metaScreen.textContent = `${accumulatorAnswerDisplay} =`;
+  accumulatorScreen.textContent = "Ans";
+  accumulatorAnswerDisplay = "Ans";
+};
 
 add = () => {
   console.log("add");
@@ -50,7 +185,5 @@ operate = (operand, num1, num2) => {
     case "divide":
       divide(num1, num2);
       break;
-    default:
-      console.log(`Sorry, ${operand} not recognized.`);
   }
 };
